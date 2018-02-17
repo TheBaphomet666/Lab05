3fbc6232eb3b40421219055c1b0745bb21e7d020
Oscar Pinto <moka117@hotmail.com>
2018-02-17 12:19:42 -0500

    Agregados casos de prueba para Registrarcliente

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -6,6 +6,9 @@
 package edu.eci.pdsw.samples.tests;
 
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquilerItemsStub;
+import edu.eci.pdsw.samples.entities.Cliente;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -27,6 +30,43 @@ public class ClientesTest {
     @Test
     public void additems1() throws ExcepcionServiciosAlquiler{
     	
+    }
+    /**
+ * 
+ * Registro de Clientes:
+ * 
+ * 
+ * 
+ * Clases de equivalencia:
+ * CE1: Cliente Ya registrado
+ * CE2:Cliente sin registrar
+ * 
+ * 
+ * 
+ */
+    @Test
+    public void registrarclientetest1(){
+        ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
+        Cliente cl=new Cliente("Bruce Willis",11519631,"666","Calle Falsa 123","example@example.com");
+        try{sa.registrarCliente(cl);
+            assertEquals(sa.consultarCliente(11519631),cl);
+        }
+        catch(ExcepcionServiciosAlquiler e){
+        fail();
+        }
+
+    }
+    @Test
+    public void registrarclientetest2(){
+        ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
+        Cliente cl=new Cliente("Bruce Willis",11519631,"666","Calle Falsa 123","example@example.com");
+        try{sa.registrarCliente(cl);
+            sa.registrarCliente(cl);
+        }
+        catch(ExcepcionServiciosAlquiler e){
+        assertEquals(e.getMessage(),"El cliente con documento "+cl+" ya esta registrado.");
+        }
+
     }
     
     
