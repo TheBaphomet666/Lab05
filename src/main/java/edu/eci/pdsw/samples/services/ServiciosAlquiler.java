6c716a1fd56cbd7d21409829e9df0cf50a4bbb11
Martin9958 <andres.cantor-u@mail.escuelaing.edu.co>
2018-02-18 11:00:06 -0500

    Correccion documentacion Trows registrar cliente

--- src/main/java/edu/eci/pdsw/samples/services/ServiciosAlquiler.java
+++ src/main/java/edu/eci/pdsw/samples/services/ServiciosAlquiler.java
@@ -71,7 +71,7 @@ public abstract class ServiciosAlquiler {
      * @param numdias el número de días que se le prestará el item
      * @pos el item ya no debe estar disponible, y debe estar asignado al
      * cliente
-     * @throws ExcepcionXX si el identificador no corresponde con un item, o si
+     * @throws ExcepcionServiciosAlquiler si el identificador no corresponde con un item, o si
      * el mismo ya esta alquilado
      */
     public abstract void registrarAlquilerCliente(Date date, long docu, Item item, int numdias) throws ExcepcionServiciosAlquiler;
